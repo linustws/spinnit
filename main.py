e4bbf9bc1a4f9f4d73a5c32c9cda03094eeddb52
Linus Tan <linustws00@gmail.com>
2023-04-20 20:51:11 +0800

    test posterised

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from telegram.ext import MessageHandler
 from telegram.ext import filters
 
 from logger import Logger
-from unposterised_no_mp_optimised import SpinnerGifMaker
+from posterised_no_mp import SpinnerGifMaker
 
 telegram_logger = Logger('telegram.ext._application', 'help_joy_decide.log')
 main_logger = Logger('main', 'help_joy_decide.log')
